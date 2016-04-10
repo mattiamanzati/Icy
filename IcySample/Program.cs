069888802b82c3cede5d275f3e5eb9f92e7aa101
mattiamanzati <mattia.manzati@gmail.com>
2016-04-10 19:29:04 +0200

    init sample

--- IcySample/Program.cs
+++ IcySample/Program.cs
@@ -13,16 +13,29 @@ namespace IcySample
 
     class Program
     {
+        static IcyApp app;
+        static DatabaseManager dm;
+
         static void Main(string[] args)
         {
-            var app = new IcyApp();
+            // initialize the app
+            app = new IcyApp();
 
+            // put some config
             app.config<ApplicationDatabaseConfig>().defaults = "test";
             app.config<ApplicationDatabaseConfig>().connections["test"] = new ApplicationDatabaseConnectionConfig()
             {
                 host = "localhost",
-                database = 
+                database = "TEST",
+                username = "sa",
+                password = "" 
             };
+
+            // initialize the database manager
+            dm = new DatabaseManager(app, new Icy.Database.Connectors.ConnectionFactory());
+
+            // perform queries! :D
+            dm.connection().query().select().from("table").where("column", "=", 1).first();
         }
     }
 }
